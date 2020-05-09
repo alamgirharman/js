4c609f3482042fa8880ad38d55c5a44e5fa8d6ee
alamgirharman <alamgirharman@gmail.com>
2020-05-10 05:53:37 +0600

    Finish c1

--- c1/c1.js
+++ c1/c1.js
@@ -1,3 +1,47 @@
 // ************************
 
 // JavaScript All You Need to Know
+
+
+
+/* 
+*** 3
+
+** Always use notebook
+** Always type code in text editor
+** Find related examples and create example
+** Spend some moment on each chapter
+** Discuss with others what you have learned
+
+
+*** Architecture
+- Programming Basics
+- Logic and Conditions
+- Loops and Repetition
+- Handling Strings
+- Arrays
+- Objects
+- Functions
+- Functional Programming
+- Scope and Closure
+- OPP Intro
+- OOP with JS
+- Prototype
+- Prototypical Inheritance
+- ES6
+- Error Handling
+- Asynchronous Programming
+- DOM
+- Project
+- JavaScript Libraries
+- Future Road map
+
+
+*** Environment
+- Browser
+- Text Editor: VS Code, Atom, Brackets
+- Download and install nodejs
+
+- Terminal: cmder (windows) / gitbash (any) /iTerm terminal for mac
+
+*/
