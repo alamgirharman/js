0778494237e831e90df929ce59e421ed8b86d34f
alamgirharman <alamgirharman@gmail.com>
2020-05-10 06:45:32 +0600

    c2...

---
+++ c2/n8.js
@@ -0,0 +1,34 @@
+// Variable
+// Storing data
+
+var name = 'HM Nayem';
+var age = 25;
+
+// console.log(name);
+// console.log(age);
+
+console.log(name + ' knows JavaScript.');
+console.log('His age is only ' + age);
+console.log(name + ' is creating a JavaScript Course for all');
+console.log('But his age is only ' + age);
+
+
+// **********************
+// Reserved word
+/* 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar
+
+*/
+
+// case/ lowerCamelCase/ CamelCase
+var mathNumber = 10;
+var accountNumberDetailsId = 104;
+var account_number_details_id = 105;
+
+
+
+
+
+
+
+
---
+++ c2/n9.js
@@ -0,0 +1,19 @@
+// Data Type
+/* 
+- Primitive Data Type
+    - Number
+    - String
+    - Boolean
+        -- true -- false
+    - undefined
+    - null
+    (from js language)
+
+
+- Object (non-primitive)
+    - Array
+    - Object
+    - Function
+*/
+
+console.log('Single quote' + "double quote");
