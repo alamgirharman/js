8d64c15d4e8da2afc3205c9e840c7dc9e177a627
alamgirharman <alamgirharman@gmail.com>
2020-05-10 06:28:30 +0600

    finish n7

---
+++ c2/n6.js
@@ -0,0 +1,20 @@
+console.log('Hello World!');
+console.log('I love Bangladesh');
+console.log(45);
+console.log(60.789);
+
+/* 
+Hello World!
+I love Bangladesh
+45
+60.789
+*/
+
+
+console.log('My Fav No: ' + 9);
+// My Fav No: 9
+
+console.log(9 + 9); // 18
+// + operator (about concatenation)
+
+console.log('9' + 9); // 9
---
+++ c2/n7.js
@@ -0,0 +1,34 @@
+// Variable
+// Storing data
+
+var name = 'HM Nayem';
+var age = 25;
+
+// console.log(name);
+// console.log(age);
+
+console.log(name + ' knows JavaScript.');
+console.log('His age is only ' + age);
+console.log(name + ' is creating a JavaScript Course for all');
+console.log('But his age is only ' + age);
+
+
+/* 
+console.log('HM Nayem knows JavaScript.');
+console.log('His age is only 23');
+console.log('HM Nayem is creating a JavaScript Course for all');
+console.log('But his age is only 23'); 
+*/
+
+// *** WV
+
+
+
+
+
+
+
+
+
+
+
